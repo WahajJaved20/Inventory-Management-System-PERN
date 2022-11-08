c4a58caf0890714d81843537d46d813abc929a9f
WahajJaved20 <k200208@nu.edu.pk>
2022-11-08 22:04:15 +0500

    login_screen_upload

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,33 +1,209 @@
-import React from "react";
-import "./login.css";
+import React, { useEffect } from "react";
+import AuthNavbar from "../components/auth/navbar";
+import {
+	Typography,
+	FormControl,
+	Box,
+	Button,
+	InputLabel,
+	OutlinedInput,
+	IconButton,
+	InputAdornment,
+} from "@mui/material";
 import { Stack } from "@mui/system";
-import { Grid, Typography, Button } from "@mui/material";
-import InventoryIcon from "@mui/icons-material/Inventory";
+import PersonIcon from "@mui/icons-material/Person";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import PasswordIcon from "@mui/icons-material/Password";
+import "./login.css";
+
 function Login() {
+	const [username, setUsername] = React.useState("");
+	const [showPassword, setShowPassword] = React.useState(false);
+	const [password, setPassword] = React.useState("");
+	function handleClickShowPassword() {
+		setShowPassword(!showPassword);
+	}
+	useEffect(() => {}, [username, showPassword, password]);
 	return (
-		<div className="navbarL">
-			<Stack direction={"row"}>
-				<Grid sx={{ mt: 3 }} container spacing={1}>
-					<Grid item xs={2}>
-						<InventoryIcon
-							style={{ color: "white" }}
-							fontSize="large"
-						/>
-					</Grid>
-					<Grid
-						item
-						xs={2}
-						paddingBottom={20}
-						sx={{ ml: -15, mt: -0.5 }}>
-						<Typography variant="h4" style={{ color: "white" }}>
-							IVMS
+		<div>
+			<AuthNavbar />
+			<div className="Form">
+				<Stack direction={"column"}>
+					<Stack direction={"row"}>
+						<Typography
+							variant="h5"
+							style={{
+								marginLeft: 90,
+								marginTop: 60,
+								color: "white",
+								fontWeight: "bold",
+								fontSize: "50px",
+								textAlign: "center",
+							}}>
+							Log In To Your Account
 						</Typography>
-					</Grid>
-					<Grid item xs={2}>
-						<Button variant="text">Text</Button>
-					</Grid>
-				</Grid>
-			</Stack>
+						<Typography
+							variant="h3"
+							style={{
+								marginLeft: 3,
+								marginTop: 37,
+								color: "blue",
+								fontSize: "80px",
+								fontFamily: "Arial",
+								textAlign: "center",
+							}}>
+							.
+						</Typography>
+					</Stack>
+					<FormControl
+						variant="standard"
+						sx={{ marginLeft: 10, marginTop: 5 }}>
+						<InputLabel
+							style={{ fontSize: 20 }}
+							sx={{
+								color: "white",
+								fontSize: 20,
+								marginLeft: 8,
+							}}>
+							Username
+						</InputLabel>
+						<Box
+							sx={{
+								marginTop: 6,
+								display: "flex",
+								alignItems: "flex-end",
+							}}>
+							<PersonIcon
+								sx={{
+									color: "white",
+									mr: 1,
+									my: 0.5,
+									fontSize: "50px",
+								}}
+							/>
+							<OutlinedInput
+								InputProps={{
+									disableUnderline: true,
+								}}
+								InputLabelProps={{
+									style: { color: "#fff", fontSize: 20 },
+								}}
+								onChange={(e) => {
+									setUsername(e.target.value);
+								}}
+								sx={{
+									backgroundColor: "#525461",
+									borderRadius: 4,
+									width: 400,
+									fontSize: 25,
+									height: 60,
+									color: "#FFFFFF",
+								}}
+							/>
+						</Box>
+					</FormControl>
+					<FormControl
+						variant="standard"
+						sx={{ marginLeft: 10, marginTop: 5 }}>
+						<InputLabel
+							style={{ fontSize: 20 }}
+							size="normal"
+							sx={{
+								color: "#FFFFFF",
+								fontSize: 20,
+								marginLeft: 8,
+							}}>
+							Password
+						</InputLabel>
+						<Box
+							sx={{
+								marginTop: 6,
+								display: "flex",
+								alignItems: "flex-end",
+							}}>
+							<PasswordIcon
+								sx={{
+									color: "white",
+									mr: 1,
+									my: 0.5,
+									fontSize: "50px",
+								}}
+							/>
+							<OutlinedInput
+								InputProps={{
+									color: "#fff",
+								}}
+								type={showPassword ? "text" : "password"}
+								value={password}
+								onChange={(e) => {
+									setPassword(e.target.value);
+								}}
+								sx={{
+									backgroundColor: "#525461",
+									borderRadius: 4,
+									width: 400,
+									fontSize: 25,
+									height: 60,
+									color: "#FFFFFF",
+								}}
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											sx={{
+												color: "white",
+												mr: 1,
+												my: 0.5,
+												fontSize: "50px",
+											}}
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											edge="end">
+											{showPassword ? (
+												<VisibilityOff />
+											) : (
+												<Visibility />
+											)}
+										</IconButton>
+									</InputAdornment>
+								}
+							/>
+						</Box>
+					</FormControl>
+					<Box
+						sx={{
+							marginTop: 5,
+							display: "flex",
+							alignItems: "flex-end",
+						}}>
+						<Button
+							variant="contained"
+							sx={{
+								backgroundColor: "#525461",
+								width: 130,
+								marginLeft: 32,
+								height: 50,
+								borderRadius: 6,
+								fontSize: 20,
+								fontWeight: "bold",
+							}}>
+							Sign Up
+						</Button>
+						<Button
+							variant="contained"
+							sx={{
+								width: 130,
+								marginLeft: 2,
+								height: 50,
+								borderRadius: 6,
+								fontSize: 20,
+								fontWeight: "bold",
+							}}>
+							Sign In
+						</Button>
+					</Box>
+				</Stack>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/auth/navbar.js
@@ -0,0 +1,30 @@
+import { Stack } from "@mui/system";
+import { Grid, Typography } from "@mui/material";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import "./navbar.css";
+function AuthNavbar() {
+	return (
+		<div className="navbarL">
+			<Stack direction={"row"}>
+				<Grid sx={{ mt: 3 }} container spacing={1}>
+					<Grid item xs={2}>
+						<InventoryIcon
+							style={{ color: "white" }}
+							fontSize="large"
+						/>
+					</Grid>
+					<Grid
+						item
+						xs={2}
+						paddingBottom={20}
+						sx={{ ml: -18, mt: -0.5 }}>
+						<Typography variant="h4" style={{ color: "white" }}>
+							IVMS
+						</Typography>
+					</Grid>
+				</Grid>
+			</Stack>
+		</div>
+	);
+}
+export default AuthNavbar;
